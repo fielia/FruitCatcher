6ce17c520de72c9cd047c49f9bfae96545a9dcd1
Rit Garg <100240647+fielia@users.noreply.github.com>
2024-01-31 08:27:22 -0500

    base code (edited slightly)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
-# 	Author:       ritvi                                                        #
-# 	Created:      1/15/2024, 12:03:14 PM                                       #
+# 	Author:       glewin                                                       #
+# 	Created:      9/11/2023, 8:28:37 AM                                        #
 # 	Description:  V5 project                                                   #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
@@ -10,17 +10,58 @@
 # Library imports
 from vex import *
 
-# The controller
-controller = Controller()
 # Brain should be defined by default
-brain = Brain()
+brain=Brain()
 
-left_motor = Motor(Ports.PORT1, 0.2, True)
-right_motor = Motor(Ports.PORT10, 0.2, False)
-arm_motor = Motor(Ports.PORT8, 0.2, True) # take a look at params for validity
-button = Bumper(brain.three_wire_port.d)
-range_finder = Sonar(brain.three_wire_port.e) # NOTE: has a range of 30 to 3000 MM
+left_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+right_motor = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
 
-drive_motors = MotorGroup(left_motor, right_motor)
+## define the colors; we'll use the default sensitivity of 3.0
+## you don't have to retrain the camera to use different sensitivities; you can just change the value here
+ORANGE_FRUIT = Signature (1, 4915, 6305, 5610, -2305, -1901, -2103, 3.0, 0)
+LIME = Signature (2, -7385, -6527, -6956, -4551, -3459, -4005, 3.0, 0)
+LEMON = Signature (3, 1907, 2409, 2158, -3619, -3129, -3374, 3.0, 0)
 
-arm_motor.set_velocity(150)
+## define the camera on port 3; the library says the colors are optional -- you do you
+Vision3 = Vision (Ports.PORT3, 72, ORANGE_FRUIT, LIME, LEMON)
+
+brain.screen.print("Hello V5")
+
+ROBOT_IDLE = 0
+ROBOT_SEARCHING = 1
+
+## start in the idle state
+state = ROBOT_IDLE
+
+def handleButtonPress():
+    global state
+    if(state == ROBOT_IDLE):
+        print('IDLE -> SEARCHING')
+        state = ROBOT_SEARCHING
+        left_motor.spin(FORWARD, 30)
+        right_motor.spin(FORWARD, -30)
+
+    else:
+        print(' -> IDLE')
+        left_motor.stop()
+        right_motor.stop()
+
+button5 = Bumper(Ports.PORT5)
+button5.pressed(handleButtonPress)
+
+def handleObjects(objects): ## this code doesn't do anything with the objects here, but you could
+    cx = Vision3.largest_object().centerX
+    cy = Vision3.largest_object().centerY
+
+    print(cx, cy)
+
+while True:
+    '''
+    These lines correspond to a checker-handler: check for an object and handle it if there is one.
+    '''
+    objects = Vision3.take_snapshot(ORANGE_FRUIT)
+    if objects:
+        handleObjects(objects)
+
+    ## a little nap to not overwhelm the system -- the frame rate will be well under 50 Hz anyway
+    sleep(20)
