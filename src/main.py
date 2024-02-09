a698a7eb3fcb9f7af2aa2907a62bf0cf2c9a2efd
Owen Miller <ormiller@wpi.edu>
2024-02-09 14:24:18 -0500

    remapped drive motors again

--- src/main.py
+++ src/main.py
@@ -49,10 +49,10 @@ controller = Controller()
 brain = Brain()
 
 # motor names are based on top-down view with proper orientation
-northwest_motor: Motor = Motor(Ports.PORT20, 0.2, False) # set boolean so motor spins towards the front of the robot
-northeast_motor: Motor = Motor(Ports.PORT19, 0.2, False) # set boolean so motor spins towards the front of the robot
-southwest_motor: Motor = Motor(Ports.PORT18, 0.2, True) # set boolean so motor spins towards the front of the robot
-southeast_motor: Motor = Motor(Ports.PORT17, 0.2, True) # set boolean so motor spins towards the front of the robot
+northwest_motor: Motor = Motor(Ports.PORT10, 0.2, False) # set boolean so motor spins towards the front of the robot
+northeast_motor: Motor = Motor(Ports.PORT9, 0.2, False) # set boolean so motor spins towards the front of the robot
+southwest_motor: Motor = Motor(Ports.PORT8, 0.2, True) # set boolean so motor spins towards the front of the robot
+southeast_motor: Motor = Motor(Ports.PORT7, 0.2, True) # set boolean so motor spins towards the front of the robot
 negative_motors: MotorGroup = MotorGroup(northwest_motor, southeast_motor) # the motors on the negative diagonal
 positive_motors: MotorGroup = MotorGroup(northeast_motor, southwest_motor) # the motors on the positive diagonal
 
@@ -70,7 +70,7 @@ def activate_control():
 		northeast_motor.spin(FORWARD, forward_speed - right_speed + spin_speed, RPM)
 		southwest_motor.spin(FORWARD, forward_speed - right_speed - spin_speed, RPM)
 		southeast_motor.spin(FORWARD, forward_speed + right_speed - spin_speed, RPM)
-#		arm_motor.spin(FORWARD, rotate_arm(), RPM)
-#		claw_motor.spin(FORWARD, move_claw(), RPM)
+		arm_motor.spin(FORWARD, rotate_arm(), RPM)
+		claw_motor.spin(FORWARD, move_claw(), RPM)
 
 activate_control()
