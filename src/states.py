a3ff9a8211a79e855e49ac87958d1a053bd743d1
Rit Garg <100240647+fielia@users.noreply.github.com>
2024-02-24 20:57:07 -0500

    adding more state logic

--- src/states.py
+++ src/states.py
@@ -1,5 +1,5 @@
 from vex import *
-from movement import Log, drive, rotate, move_arm, move_claw, toggle_squeeze, toggle_door, kill, reset_motors, reach_wall, drop_fruit, reach_wall, controller, imu, brain
+from movement import Log, drive, rotate, move_arm, move_claw, toggle_squeeze, toggle_door, kill, reset_motors, reach_wall, go_to_bin_position, drop_fruit, reach_wall, controller, imu, brain
 from routes import go_to_tree
 from fruits import orchard, get_fruit
 
@@ -19,17 +19,42 @@ def calibrate_sensors():
 	brain.screen.clear_screen()
 	brain.screen.print_at("Button Ready", x=50, y=50)
 
-# temporary function that contains TRAVELING and GRABBING code
-def activate_auto():
-	"""
-	What the robot executes.
-	"""
-	while controller.buttonA.pressing():
-		wait(5)
-	current_tree: tuple[int, int] = (0, 0)
+current_tree: tuple[int, int] = (0, 0)
+
+def travel_to_next_tree(trees_visited: int):
+	global current_tree
+	
+	if trees_visited == 0:
+		current_tree = (0, 0)
+	elif trees_visited == 1:
+		current_tree = (0, 1)
+	elif trees_visited == 2:
+		current_tree = (0, 2)
+	elif trees_visited == 3:
+		current_tree = (2, 0)
+	elif trees_visited == 4:
+		current_tree = (2, 1)
+	elif trees_visited == 5:
+		current_tree = (2, 2)
+	elif trees_visited == 6:
+		current_tree = (2, 0)
+	elif trees_visited == 7:
+		current_tree = (2, 1)
+	elif trees_visited == 8:
+		current_tree = (2, 2)
+
 	go_to_tree(current_tree)
+
+def obtain_fruit():
 	get_fruit(current_tree)
-	Log.return_to_origin()
+
+def return_to_bins():
+	reached: bool = False
+	while not reached:
+		reached = reach_wall()
+	reached = False
+	while not reached:
+		reached = go_to_bin_position(orchard.get_tree_color(current_tree))
 
 ### end of state functions
 
